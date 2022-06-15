d4cc027d154a0bea663d156d50d76abf27d9e335
Aaryan <singhaaryan376@gmail.com>
2022-06-15 05:39:18 +0530

    Added App components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import Card from './Components/Card';
+import Loader from './Components/Loader';
 import './App.css';
 
+const category = [
+
+  'all','national','business','sports','world','politics','technology','startup','entertainment','miscellaneous','hatke','science', 'automobile']
+
 function App() {
+   
+  const [data,setData] = useState(null);
+  const[selected,setSelected]  = useState('all');
+
+ 
+  
+  useEffect(()=>{
+    async function getNews(){
+      setData(null);
+      const res =  await fetch(`https://inshorts.deta.dev/news?category=${selected}`);
+      const response  = await res.json();
+      setData(response.data);
+    }
+    getNews()
+  },[selected])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+        
+         <div className='container text-center pt-5 pb-3'>
+          <h1>NewsNow</h1>
+          
+         </div>
+         <div className='container'>
+           {category.map((item,index)=>(
+              <button  key = {index} className={selected === item ? ('btn btn-primary me-2') : ('btn btn-outline-primary me-2')} onClick = {()=>{
+               setSelected(item);
+             }}>{item}</button>
+           ))}
+           </div>
+         
+         {data == null ? (
+               <Loader />
+          
+         ): (
+         <div className='container mt-5'>
+            {data.map((item,index)=> (
+                <Card key = {index} item = {item}/>)
+          )}
+           
+          
+         </div>)}
     </div>
   );
 }
---
+++ src/Components/Card.js
@@ -0,0 +1,26 @@
+
+
+export default function Card(props){
+    return (
+         <div>
+         <div className="card mb-3" >
+          <div className="row g-0">
+           <div className="col-md-4">
+           <img src={props.item.imageUrl} className="img-fluid rounded-start " alt="..."/>
+         </div>
+         <div className="col-md-8">
+         <div className="card-body">
+        <h5 className="card-title">{props.item.title}</h5>
+        <p className="card-text">{props.item.content}</p>
+        <div className="d-flex flex-row justify-content-between" >
+        <p className="card-text"><small class="text-muted">{props.item.data}</small></p>
+        <a className="btn btn-primary" rel = "noreferrer" href={props.item.readMoreUrl} target = "_blank">Read more</a>
+        </div>
+        
+      </div>
+    </div>
+  </div>
+</div>
+         </div>
+    )
+}
